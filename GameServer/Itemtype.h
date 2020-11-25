faaf771755d213085e84c6a215c7a0a093951b76
zun chen <zun.chen666@gmail.com>
2020-11-25 23:17:20 +0800

    fix

---
+++ GameServer/Itemtype.h
@@ -0,0 +1,19 @@
+enum EltemType
+{
+    Money,
+    eCONSUME,
+    eEQUIP,
+};
+
+enum eltemAttributeType
+{
+    eITEM_ATTRIBUTE_ATK,
+    eITEM_ATTRIBUTE_HP,
+};
+
+enum eltemModuleType
+{
+    eITEM_MODULE_BASE,
+    eITEM_MODULE_POWER,
+    eITEM_MODULE_INSERT_ITEM,
+};
